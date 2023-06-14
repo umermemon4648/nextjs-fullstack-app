16da7f20b3aab1b6649e33f6f1a91cbbf53d0a16
umermemon4648 <umermemon4648@gmail.com>
2023-06-14 05:10:13 +0500

    update api end-point created

--- src/pages/api/updateUser/index.js
+++ src/pages/api/updateUser/index.js
@@ -6,8 +6,20 @@ export default async function handler(req,res){
     if (req.method==='PUT') {
         const {fname, lname, email, gender} = req.body
         try {
-            await User.findByIdAndDelete({_id: req.query.id})
-            return res.status(200).json({success: true ,message:'Successfully Deleted'})
+            const existingUser = await User.findById({_id: req.query.id})
+            if (!existingUser) {
+                return res.status(400).json({success: false ,message:'Item not found'})
+            }
+
+            // update the fields
+            existingUser.fname = fname
+            existingUser.lname = lname
+            existingUser.email = email
+            existingUser.gender = gender
+
+            // save updated User
+            const updatedUser = await existingUser.save()
+            return res.status(200).json({success: true ,updatedUser})
 
         } catch (error) {
             return res.status(500).json({error:'Failed to fetch doc'})
